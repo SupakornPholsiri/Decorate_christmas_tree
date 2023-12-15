0ab6f365c29bd6792d5cce11756a210ce6feed02
SupakornPholsiri <supakorncpw@gmail.com>
2023-12-15 17:21:07 +0700

    Chrismas Tree functionality done
    Decorations can be put on each spots individually and can be removed from all spots at once.

---
+++ christmas_tree/ChristmasTree.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+# For testing
+@onready var decor1 = preload("res://decorations/mini_present.tres")
+@onready var decor2 = preload("res://decorations/candy_cane.tres")
+func decorations_tests():
+	 # Put up decorations on all empty spots
+	for decor_spot in $Decorations.get_children():
+		decor_spot.set_decor(decor1)
+	await get_tree().create_timer(1).timeout
+	 # Try to put up decorations on all spots
+	for decor_spot in $Decorations.get_children():
+		decor_spot.set_decor(decor2)
+	await get_tree().create_timer(1).timeout
+	 # Remove decorations from the tree
+	remove_decorations()
+	await get_tree().create_timer(1).timeout
+	 # Put up decorations on all empty spots
+	for decor_spot in $Decorations.get_children():
+		decor_spot.set_decor(decor2)
+	await get_tree().create_timer(2).timeout
+	 # Remove decorations from the tree
+	remove_decorations()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	decorations_tests()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+# Remove all decorations from the tree.
+func remove_decorations():
+	for decor_spot in $Decorations.get_children():
+		decor_spot.remove_decor()
---
+++ christmas_tree/decor_spot.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+var decor : Decoration = null
+@onready var no_decor_texture : Texture2D = $Sprite2D.texture
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+# Call this function from the object that can put up decorations.
+# Also return whether the decoration was put or not
+func set_decor(new_decor : Decoration):
+	if decor == null:
+		decor = new_decor
+		$Sprite2D.texture = decor.texture
+		# Successfully put up the decoration.
+		return true
+	# Another decoration already up.
+	return false
+	
+func remove_decor():
+	decor = null
+	$Sprite2D.texture = no_decor_texture
