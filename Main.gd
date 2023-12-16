d70bd7ef0dbf34e656a4bf618fce041e818f4ba5
SupakornPholsiri <supakorncpw@gmail.com>
2023-12-16 21:56:38 +0700

    Back button

--- Main.gd
+++ Main.gd
@@ -3,12 +3,13 @@ extends Node
 @onready var timer = $Timer
 @onready var world = $World
 @onready var ingame_nodes = $World/InGameNodes
-@onready var audino = $AudioStreamPlayer2D
+@onready var audio = $AudioStreamPlayer2D
 @onready var ui_timer = $UI/Time
+@onready var back = $UI/Back
 
 func _ready():
 	start_game()
-	audino.play()
+	audio.play()
 
 func _process(delta):
 	ui_timer.text = "%d" % timer.time_left
@@ -20,6 +21,10 @@ func end_game():
 	ingame_nodes.visible = false
 	world.set_process(false)
 	world.set_physics_process(false)
+	back.visible = true
 	
 func _on_timer_timeout():
 	end_game()
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://main_menu.tscn")
