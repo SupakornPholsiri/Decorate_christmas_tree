dbb3f382cdc9e2ffcf6b8bec58a663595322e14d
JedsadaSrijunpoe <tae456852@gmail.com>
2023-12-17 19:59:51 +0700

    Add highlight to ladder

--- ladder/ladder.gd
+++ ladder/ladder.gd
@@ -6,10 +6,14 @@ var ladder_height: float
 var is_ladder_ready: bool
 @export_enum("bottom", "top") var ladder_type: String
 
+@onready var sprite2D = get_node("../Sprite2D")
+@onready var texture : Texture2D = sprite2D.texture
+@onready var texture_highlight : Texture2D = preload("res://ladder/Ladder_highlight.png")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	is_ladder_ready = true
-	ladder_height = get_node("../Sprite2D").region_rect.size.y - 20
+	ladder_height = sprite2D.region_rect.size.y - 20
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,3 +25,14 @@ func _on_check_player_area_body_entered(body):
 
 func _on_check_player_area_body_exited(body):
 	is_ladder_ready = true
+
+func highlight_area(area: Area2D):
+	if not is_ladder_ready:
+		return
+	if self == area:
+		sprite2D.texture = texture_highlight
+	else:
+		sprite2D.texture = texture
+
+func _on_body_exited(body):
+	sprite2D.texture = texture
